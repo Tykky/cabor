ef6b3b7059e6ac4c3ba413b277846f015a04b7bc
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-11 01:21:25 +0200

    asd

--- ci/win2022.bat
+++ ci/win2022.bat
@@ -2,6 +2,7 @@
 
 # Assume chocolatey is already installed
 choco install cmake visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK"
+RefreshEnv.cmd
 call build.bat
 
 exit /B 0
