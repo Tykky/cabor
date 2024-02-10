7559e815e9d6eeb6708b0a3d1df362503c9777e7
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-10 17:16:37 +0200

    cmake and build scripts

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-cabor
+# Cabor
+
+
+## How to build
+
+The build dependencies are
+- CMake 3.22 or newer
+- C99 compiler
+
+to build on Windows use
+```
+build
+```
+to build on Linux use
+```
+./build.sh
+```
---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+where /q cmake
+if ERRORLEVEL 1 (
+    ECHO "cmake.exe not found, please add cmake to PATH and make sure cmake is installed"
+    exit 1
+)
+
+if not exist build mkdir build
+cd build
+
+cmake ..
+ECHO -- Building cabor in Debug configuration
+cmake --build . --config Debug
+ECHO -- Building cabor in Release configuration
+cmake --build . --config Release
+
+cd ..
+exit 0
