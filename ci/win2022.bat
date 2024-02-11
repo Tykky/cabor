8a38b38b0029c2aa9b732ac743f56922bf9376ed
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-11 14:28:27 +0200

    update

--- ci/win2022.bat
+++ ci/win2022.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PATH=%PATH%;C:\"Program Files"\Git\bin
-set PATH=%PATH%;C:\"Program Files"\CMake\bin
-set PATH=%PATH%;C:\"Program Files (x86)"\"Microsoft Visual Studio"\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64
+setx PATH=%PATH%;C:\"Program Files"\Git\bin
+setx PATH=%PATH%;C:\"Program Files"\CMake\bin
+setx PATH=%PATH%;C:\"Program Files (x86)"\"Microsoft Visual Studio"\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64
 
 choco install cmake visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK" -y
 RefreshEnv.cmd
