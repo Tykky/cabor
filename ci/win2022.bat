fae9fb2a664266e88348899f90b89a9a49177f8c
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-12 01:24:47 +0200

    fix

--- ci/win2022.bat
+++ ci/win2022.bat
@@ -1,7 +1,7 @@
 @echo off
 
 choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
-choco install visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64" -y
+choco install visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK --add Microsoft.VisualStudio.Component.Windows10SDK.18362 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 --add Microsoft.VisualStudio.Component.Windows10SDK.20348 --add Microsoft.VisualStudio.Component.Windows11SDK.22000" -y
 refreshenv
 
 exit 0
