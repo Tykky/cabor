0225449d56e0c24ceeb17710276cca61efab7c7e
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-11 21:07:01 +0200

    XD

--- ci/win2022.bat
+++ ci/win2022.bat
@@ -2,7 +2,11 @@
 
 choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
 choco install visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools -y
+
 refreshenv
+
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+
 call build.bat
 
 exit 0
