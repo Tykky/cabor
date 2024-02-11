3a04847ffd09230af43f3449125fe6b482a5eb25
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-11 14:38:04 +0200

    add asd

--- build.bat
+++ build.bat
@@ -4,12 +4,6 @@ set PATH=%PATH%;C:\"Program Files"\Git\bin
 set PATH=%PATH%;C:\"Program Files"\CMake\bin
 set PATH=%PATH%;C:\"Program Files (x86)"\"Microsoft Visual Studio"\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64
 
-where /q cmake
-if ERRORLEVEL 1 (
-    ECHO "cmake.exe not found, please add cmake to PATH and make sure cmake is installed"
-    exit /B 1
-)
-
 if not exist build mkdir build
 cd build
 
