10f3551fd3d6157a5ae3ab59490d569d2aa728bf
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-12 00:26:51 +0200

    asd

--- ci/ubuntu202204LTS.sh
+++ ci/ubuntu202204LTS.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-echo "-- installing build dependencies"
-
 apt -y update
 apt -y upgrade
 apt -y install cmake
-
-echo "-- running build.sh"
-bash build.sh
 
 exit 0
--- ci/win2022.bat
+++ ci/win2022.bat
@@ -2,11 +2,6 @@
 
 choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
 choco install visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64" -y
-
 refreshenv
-
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-
-call build.bat
 
 exit 0
