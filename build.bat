01a80da2e5de65fb3e993fe78627b455a1628fcb
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-11 17:05:57 +0200

    asd

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set PATH=%PATH%;C:\"Program Files"\Git\bin
-set PATH=%PATH%;C:\"Program Files"\CMake\bin
+git=C:\"Program Files"\Git\bin\git.exe
+cmake=C:\"Program Files"\CMake\bin\cmake.exe
 set PATH=%PATH%;C:\"Program Files (x86)"\"Microsoft Visual Studio"\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64
 
 if not exist build mkdir build
