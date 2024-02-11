f2f07cee910b7ebcd830b179da59cd064c6a84f8
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-11 20:52:13 +0200

    script

--- ci/win2022.bat
+++ ci/win2022.bat
@@ -1,7 +1,8 @@
 @echo off
 
-choco install cmake visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK" -y
-RefreshEnv.cmd
+choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
+choco install visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK" -y
+refreshenv
 call build.bat
 
-exit /B 0
+exit 0
