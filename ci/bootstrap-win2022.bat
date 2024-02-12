fcccf60a08a3b9df7052550869ad4dc0ac426c30
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-12 02:25:47 +0200

    update

--- ci/bootstrap-win2022.bat
+++ ci/bootstrap-win2022.bat
@@ -1,7 +1,6 @@
 @echo off
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-refreshenv
 
 git clone https://github.com/Tykky/cabor
 
