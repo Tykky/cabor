4d980f1bc1d526ff9e134c1ad596a975c38d9f8d
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-14 00:26:12 +0200

    asd

--- ci/bootstrap-win2022.bat
+++ ci/bootstrap-win2022.bat
@@ -1,9 +1,7 @@
-@echo off
-
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install git -y
-
+refreshenv
 git clone https://github.com/Tykky/cabor
 
 cd cabor
@@ -11,5 +9,3 @@ cd cabor
 choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
 choco install visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.Windows10SDK --add Microsoft.VisualStudio.Component.Windows11SDK.22000 --includeRecommended --quiet --norestart" -y
 refreshenv
-
-exit 0
