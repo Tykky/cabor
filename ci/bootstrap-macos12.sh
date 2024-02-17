588d17b9c6d56e85b446d325830fb3a78cfdea3d
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-17 23:54:11 +0200

    update script

--- ci/bootstrap-macos12.sh
+++ ci/bootstrap-macos12.sh
@@ -2,13 +2,6 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install git cmake rbenv
-
-rbenv init
-rbenv install 3.1.2
-rbenv global 3.1.2
-
-gem install xcode-install
-xcversion install '15.2'
-sudo xcodebuild -license accept
-sudo xcode-select -s /Applications/Xcode-15.2.app
+brew install git cmake mas
+mas install 497799835
+xcode-select --install
