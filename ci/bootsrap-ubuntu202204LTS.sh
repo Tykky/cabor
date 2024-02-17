712f0657ff9a1edba22f31eb149acf3eb151db48
Tykky <40919314+Tykky@users.noreply.github.com>
2024-02-17 02:39:10 +0200

    busted

--- ci/bootsrap-ubuntu202204LTS.sh
+++ ci/bootsrap-ubuntu202204LTS.sh
@@ -2,6 +2,6 @@
 
 apt -y --force-yes update
 apt -y --force-yes upgrade
-apt -y --force-yes install git cmake gcc
+apt -y --force-yes install git cmake gcc clang
 
 git clone https://github.com/Tykky/cabor
